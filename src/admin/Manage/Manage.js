d9115c54b46b65d6f62ed47251111ad914b0715a
DiorEtMoi <dinhhoan0019c@gmail.com>
2023-03-24 11:14:02 +0700

    uptingg

--- src/admin/Manage/Manage.js
+++ src/admin/Manage/Manage.js
@@ -1,6 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { isSuccess, isFailing, isLoading } from "../../redux/auth/slice";
 
 function Manage() {
+  const [type, setType] = useState([]);
+  const [movie, setMovie] = useState([]);
+  const [rate, setRate] = useState([]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let here = true;
+    const url = "http://localhost:3000/type";
+    dispatch(isLoading());
+    axios
+      .get(url)
+      .then((res) => {
+        if (!here) {
+          return;
+        }
+        setType(res?.data);
+        console.log(res?.data);
+        dispatch(isSuccess());
+      })
+      .catch((err) => {
+        dispatch(isFailing());
+      });
+    return () => {
+      here = false;
+    };
+  }, []);
+  useEffect(() => {
+    let here = true;
+    const url = "http://localhost:3000/movie";
+    dispatch(isLoading());
+    axios
+      .get(url)
+      .then((res) => {
+        if (!here) {
+          return;
+        }
+        setMovie(res?.data);
+        console.log(res?.data);
+
+        dispatch(isSuccess());
+      })
+      .catch((err) => {
+        dispatch(isFailing());
+      });
+    return () => {
+      here = false;
+    };
+  }, []);
+  useEffect(() => {
+    let here = true;
+    const url = "http://localhost:3000/rating";
+    dispatch(isLoading());
+    axios
+      .get(url)
+      .then((res) => {
+        if (!here) {
+          return;
+        }
+        setRate(res?.data);
+        console.log(res?.data);
+
+        dispatch(isSuccess());
+      })
+      .catch((err) => {
+        dispatch(isFailing());
+      });
+    return () => {
+      here = false;
+    };
+  }, []);
   return <div>Manage</div>;
 }
 
